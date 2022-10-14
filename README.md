2b5e1b76e795180fa7c31582a6aab52a55b28452
Steven-Chang1114 <s1992413@ed.ac.uk>
2022-10-14 15:03:20 +0100

    add init example code

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # effect_handler_links
+linx --path=/Users/stevenchang/GitHub/effect_handler_links webserver.links 
---
+++ js/demo.js
@@ -0,0 +1,6 @@
+function _hello() {
+    console.log("FUNCTION TRIGGERED")
+    //return "HELLO"
+}
+
+var hello = _$Links.kify(_hello);
