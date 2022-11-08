69a4850e6487b317a207a88f6b522cc9f683968d
Steven-Chang1114 <s1992413@ed.ac.uk>
2022-11-08 22:40:48 +0000

    Add one more hook

--- src/Hook.js
+++ src/Hook.js
@@ -1,13 +1,22 @@
 import { useState } from "react";
 
 function Hook() {
-    let [num, setNum] = useState(0);
+  let [num, setNum] = useState(0);
+    let [numPrev, setNumPrev] = useState(0);
 
     return (
       <div>
-        {num}
-        <button onClick={() => setNum(1)}>set number to 1</button>
-        <button onClick={() => setNum(2)}>set number to 2</button>
+        <div>
+          {num}
+          <button onClick={() => setNum(1)}>set number to 1</button>
+          <button onClick={() => setNum(2)}>set number to 2</button>
+        </div>
+
+        <div>
+          {numPrev}
+          <button onClick={() => setNumPrev(prev => prev - 1)}>sub number by 1</button>
+          <button onClick={() => setNumPrev(prev => prev + 1)}>add number by 1</button>
+        </div>
       </div>
     );
   }
