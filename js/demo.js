dab17bca667a26aee79cb67fb02d155672dc338c
Steven-Chang1114 <s1992413@ed.ac.uk>
2022-12-08 21:19:09 +0000

    successfully make input.links lag

--- js/demo.js
+++ js/demo.js
@@ -6,5 +6,19 @@ function _btnOnCLick() {
     console.log(`CLICKED`)
 }
 
+function _printResult(res) {
+    console.log(res)
+    const container = document.querySelector("#result");
+    container.innerHTML = null
+
+    for (let i = 0; i < 100000; i++) {
+        const node = document.createElement("div");
+        node.innerHTML = res
+        container.appendChild(node)
+    }
+
+}
+
 let hello = LINKS.kify(_hello);
 let btnOnCLick = LINKS.kify(_btnOnCLick);
+let printResult = LINKS.kify(_printResult);
